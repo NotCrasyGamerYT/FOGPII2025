9ac8d1acd88903e722b9f0d0a2d4af5b90596e25
ItsCrasyGamer_YT <potatoe0201@gmail.com>
2025-09-15 10:29:40 -0500

    Made different Scenes

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public void NextScenes()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
